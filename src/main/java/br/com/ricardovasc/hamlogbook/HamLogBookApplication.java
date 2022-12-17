8c03d2da95717f1d48f7983f695921e48de48798
ricardovasc <ricardovasc@gmail.com>
2022-12-17 13:41:53 -0300

    First app commit

---
+++ src/main/java/br/com/ricardovasc/hamlogbook/HamLogBookApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricardovasc.hamlogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamLogBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamLogBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ricardovasc/hamlogbook/HamLogBookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardovasc.hamlogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamLogBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
