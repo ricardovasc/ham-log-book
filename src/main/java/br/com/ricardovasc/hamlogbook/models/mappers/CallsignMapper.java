b5fa1911a4f6c93ac3b398bac254d196efd0c295
Ricardo Vasconcelos <ricardovasc@gmail.com>
2023-12-09 12:31:54 -0300

    Ignore unmapped fields on mappers

--- src/main/java/br/com/ricardovasc/hamlogbook/models/mappers/CallsignMapper.java
+++ src/main/java/br/com/ricardovasc/hamlogbook/models/mappers/CallsignMapper.java
@@ -1,6 +1,7 @@
 package br.com.ricardovasc.hamlogbook.models.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import br.com.ricardovasc.hamlogbook.models.Callsign;
 import br.com.ricardovasc.hamlogbook.models.dtos.CallsignDTO;
@@ -8,6 +9,7 @@ import br.com.ricardovasc.hamlogbook.models.dtos.CallsignDTO;
 @Mapper(componentModel = "spring")
 public interface CallsignMapper {
 	
+	@Mapping(target = "logSheetList", ignore = true)
 	Callsign callsignDTOToCallsign(CallsignDTO callsignDTO);
 	
 	CallsignDTO callsignToCallsignDTO(Callsign callsign);
--- src/main/java/br/com/ricardovasc/hamlogbook/models/mappers/LogSheetMapper.java
+++ src/main/java/br/com/ricardovasc/hamlogbook/models/mappers/LogSheetMapper.java
@@ -1,6 +1,7 @@
 package br.com.ricardovasc.hamlogbook.models.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import br.com.ricardovasc.hamlogbook.models.LogSheet;
 import br.com.ricardovasc.hamlogbook.models.dtos.LogSheetDTO;
@@ -8,6 +9,7 @@ import br.com.ricardovasc.hamlogbook.models.dtos.LogSheetDTO;
 @Mapper(componentModel = "spring")
 public interface LogSheetMapper {
 	
+	@Mapping(target = "callsignList", ignore = true)
 	LogSheet logSheetDTOToLogSheet(LogSheetDTO logSheetDTO);
 	
 	LogSheetDTO logSheetToLogSheetDTO(LogSheet logSheet);
