34edff98d12881ab7ccdd0fafc1bfd1adb2c65ba
Ricardo Vasconcelos <ricardovasc@gmail.com>
2023-12-10 19:49:46 -0300

    Add sql files for integration test

--- src/test/java/br/com/ricardovasc/hamlogbook/integration/CallsignIntegrationTest.java
+++ src/test/java/br/com/ricardovasc/hamlogbook/integration/CallsignIntegrationTest.java
@@ -8,9 +8,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +27,10 @@ import lombok.experimental.FieldDefaults;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@SqlGroup({
+    @Sql(value = "classpath:init/callsign.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(value = "classpath:empty/reset.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD),
+})
 public class CallsignIntegrationTest {
 
     public static final String CALLSIGN_PATH = "/callsigns";
@@ -41,7 +49,11 @@ public class CallsignIntegrationTest {
                     MockMvcRequestBuilders.get(CALLSIGN_PATH)
                             .contentType(MediaType.APPLICATION_JSON));
             
-            resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+            resultActions
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$[0].code").value("PU7AAA"))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Name for PU7AAA"));
         }
     }
 }
